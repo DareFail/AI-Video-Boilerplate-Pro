3d52eab486c1bfaab1d0d9776fae22f0959b8575
James <jamespsteinberg@gmail.com>
2024-09-08 02:42:46 -0400

    design applied to pro

--- XXXXX_websockets/views/default.py
+++ XXXXX_websockets/views/default.py
@@ -2,8 +2,12 @@ from ..utils import render_with_appname
 
 
 def home(request):
-    return render_with_appname(request, "index.html", {})
+    context = {}
+    return render_with_appname(request, "index.html", context=context)
 
 
 def room(request, room_name):
-    return render_with_appname(request, "room.html", {"room_name": room_name})
+    context = {
+     "room_name": room_name   
+    }
+    return render_with_appname(request, "room.html", context=context)
--- homepage/views/default.py
+++ homepage/views/default.py
@@ -2,4 +2,5 @@ from ..utils import render_with_appname
 
 
 def home(request):
-    return render_with_appname(request, "index.html")
+    context = {}
+    return render_with_appname(request, "index.html", context=context)
