9def1d4b6a35aee60035491130d5f7488fd0dbc7
James <jamespsteinberg@gmail.com>
2024-09-13 01:36:53 -0400

    cropRotate

--- pdf/views/default.py
+++ pdf/views/default.py
@@ -14,44 +14,26 @@ def home(request):
 @csrf_exempt
 def runStep(request):
 
-    listOfImagePages = []
-
     if request.method == 'POST':
         command = request.POST.get('command', '')
 
         if command == "pdfToImagePages":
             file = request.FILES['file'].file
 
-            listOfImagePages = pdfToImagePages(file, 1)
+            listOfImagePages = pdfToImagePages(file=file, page_number=1)
 
-        '''
-        listOfText = []
-        listOfImageResults = []
-        for imagePage in listOfImagePages:
-            image_dimensions = imageWidthHeight(file=imagePage)
+            return JsonResponse({
+                "result": listOfImagePages[0],
+            })
 
-            width = image_dimensions["width"]
-            height = image_dimensions["height"]
+        elif command == "ocr":
+            image = request.POST.get('image', '')
 
-            croppedAndRotatedImage = cropRotateImage(file=imagePage, x=0, y=0, width=width, height=height, rotation_degrees=0)
-            listOfText.append(imageToText_Roboflow(file=croppedAndRotatedImage, api_key=settings.ROBOFLOW_API_KEY))
-            
-            listOfImageResults.append(croppedAndRotatedImage)
+            text = imageToText_Roboflow(file=image, api_key=settings.ROBOFLOW_API_KEY)
 
-        return JsonResponse({
-            "pages": listOfImageResults,
-            "texts": listOfText,
-        })
-        '''
-
-        result = ""
-
-        if len(listOfImagePages) > 0:
-            result = listOfImagePages[0]
-
-        return JsonResponse({
-            "result": result,
-        })
+            return JsonResponse({
+                "result": text,
+            })
 
         
     else:
