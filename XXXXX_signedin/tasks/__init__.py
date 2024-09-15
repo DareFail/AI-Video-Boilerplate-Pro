ac7a4a3d6cd6ffde401f9601f8bf5322a9dcc6e9
James <jamespsteinberg@gmail.com>
2024-09-15 00:47:37 -0400

    fix task init

---
+++ XXXXX_signedin/tasks/__init__.py
@@ -0,0 +1,3 @@
+from .email_captcha import *
+from .email import *
+from .slack import *
--- rateloaf/tasks/__init__.py
+++ rateloaf/tasks/__init__.py
@@ -1,1 +1,4 @@
 from .cats import *
+from .email_captcha import *
+from .email import *
+from .slack import *
