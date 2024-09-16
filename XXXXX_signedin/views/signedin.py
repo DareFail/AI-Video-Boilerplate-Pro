f7a66bcc8833029edf1baaae3fa146dcd610163f
James <jamespsteinberg@gmail.com>
2024-09-15 23:47:11 -0400

    fix multiline comments

--- XXXXX_signedin/views/signedin.py
+++ XXXXX_signedin/views/signedin.py
@@ -5,4 +5,8 @@ from ..utils import render_with_appname
 
 @signedin
 def signedin_home(request, group_unique):
+    
+    # uncomment if any model that needs to be made on signup with a foreign key to Group
+    # profile = Profile.objects.filter(group=request.group).get_or_create(group=request.group)
+
     return render_with_appname(request, "signedin/index.html")
