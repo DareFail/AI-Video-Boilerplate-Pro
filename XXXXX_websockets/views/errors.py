a68798764d688c21ae4933da3ada50eaefbceea0
James <jamespsteinberg@gmail.com>
2024-11-28 16:53:11 -0500

    revert

--- XXXXX_websockets/views/errors.py
+++ XXXXX_websockets/views/errors.py
@@ -2,8 +2,8 @@ from ..utils import render_with_appname
 
 def handle_404(request):
     context = {}
-    return render_with_appname(request, "404.html", context=context, status=404)
+    return render_with_appname(request, "404.html", context=context)
 
 def handle_500(request):
     context = {}
-    return render_with_appname(request, "500.html", context=context, status=500)
+    return render_with_appname(request, "500.html", context=context)
