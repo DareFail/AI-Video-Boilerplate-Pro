fe526968ecc3f577402ad8c92317ce3154d7e5c1
James <jamespsteinberg@gmail.com>
2024-11-28 16:36:49 -0500

    error status

--- pdf/views/errors.py
+++ pdf/views/errors.py
@@ -2,8 +2,8 @@ from ..utils import render_with_appname
 
 def handle_404(request):
     context = {}
-    return render_with_appname(request, "404.html", context=context)
+    return render_with_appname(request, "404.html", context=context, status=404)
 
 def handle_500(request):
     context = {}
-    return render_with_appname(request, "500.html", context=context)
+    return render_with_appname(request, "500.html", context=context, status=500)
