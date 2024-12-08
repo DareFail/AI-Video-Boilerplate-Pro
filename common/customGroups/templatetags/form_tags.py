c2e73a1a8dadc613e7f1040a2e4f5e9e4b774110
James <jamespsteinberg@gmail.com>
2024-12-07 19:14:33 -0500

    add datetime form

--- common/customGroups/templatetags/form_tags.py
+++ common/customGroups/templatetags/form_tags.py
@@ -16,6 +16,7 @@ def render_field(form_field):
         "select": render_select_input,
         "checkbox": render_checkbox_input,
         "date": render_date_input,
+        "datetime": render_datetime_input,
     }.get(form_field.widget_type, render_text_input)
     return render_function(form_field)
 
@@ -76,6 +77,20 @@ def render_checkbox_input(form_field):
     """
     return _render_field(CHECKBOX_INPUT_TEMPLATE, form_field)
 
+@register.simple_tag
+def render_datetime_input(form_field):
+    DATETIME_INPUT_TEMPLATE = """<div class="mb-3">
+      <label for="{{ form_field.id_for_label }}" class="form-label">{{ form_field.label }}</label>
+      {% if form_field.value %}
+        <input type="datetime-local" name="{{ form_field.name }}" id="{{ form_field.id_for_label }}" value="{{ form_field.value|date:"Y-m-d\\TH:i" }}">
+      {% else %}
+        <input type="datetime-local" name="{{ form_field.name }}" id="{{ form_field.id_for_label }}" value="">
+      {% endif %}
+      <small class="form-text text-muted">{{ form_field.help_text }}</small>
+      {{ form_field.errors }}
+    </div>
+    """
+    return _render_field(DATETIME_INPUT_TEMPLATE, form_field)
 
 def _render_field(template_text, form_field):
     template_object = template.Template(template_text)
