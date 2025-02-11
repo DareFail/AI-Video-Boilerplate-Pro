cb5eec6f6b606c781ed92af8e27baabf92b47a6c
James <jamespsteinberg@gmail.com>
2025-02-10 19:20:16 -0500

    fix

--- XXXXX_signedin/views/errors.py
+++ XXXXX_signedin/views/errors.py
@@ -1,9 +1,20 @@
+import logging
+
 from ..utils import render_with_appname
+
 
 def handle_404(request):
     context = {}
     return render_with_appname(request, "404.html", context=context, status=404)
 
-def handle_500(request):
+
+def handle_500(request, *args, **argv):
+
+    logger = logging.getLogger(__name__)
+
+    error = argv.get("exception", None)
+
+    logger.error("Internal Server Error: %s", error)
+
     context = {}
     return render_with_appname(request, "500.html", context=context, status=404)
